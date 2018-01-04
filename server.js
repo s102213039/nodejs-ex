a4db2d8ba44a0ea36f64e84536df667eaca7dc59
chiuyanyu <s102213039@mail1.ncnu.edu.tw>
2018-01-04 19:26:10 +0800

    fix

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 ﻿//  OpenShift sample Node application
 var express = require('express'),
-    server     = express(),
+    app     = express(),
     morgan  = require('morgan');
 
 var main 	= require('./main');
-var http 	= require('http');
-var start = http.createServer(main);
+    
 Object.assign=require('object-assign')
 
-server.use(morgan('combined'))
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -58,7 +58,25 @@ var initDb = function(callback) {
   });
 };
 
-server.get('/pagecount', function (req, res) {
+app.get('/', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('index.html', { pageCountMessage : null});
+  }
+});
+
+app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
@@ -74,7 +92,7 @@ server.get('/pagecount', function (req, res) {
 });
 
 // error handling
-server.use(function(err, req, res, next){
+app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500).send('Something bad happened!');
 });
@@ -83,7 +101,7 @@ initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
-server.listen(port, ip);
+app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
-module.exports = server ;
+module.exports = app ;
