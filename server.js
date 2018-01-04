65248a45bb182cb8dd278af8ba4cb6bec2db6b10
chiuyanyu <s102213039@mail1.ncnu.edu.tw>
2018-01-04 19:10:22 +0800

    fix

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 ﻿//  OpenShift sample Node application
 var express = require('express'),
-    app     = express(),
+    server     = express(),
     morgan  = require('morgan');
 
 var main 	= require('./main');
     
 Object.assign=require('object-assign')
 
-app.use(morgan('combined'))
+server.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -57,7 +57,7 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/pagecount', function (req, res) {
+server.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
@@ -73,7 +73,7 @@ app.get('/pagecount', function (req, res) {
 });
 
 // error handling
-app.use(function(err, req, res, next){
+server.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500).send('Something bad happened!');
 });
@@ -82,7 +82,7 @@ initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
-app.listen(port, ip);
+server.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
-module.exports = app ;
+module.exports = server ;
